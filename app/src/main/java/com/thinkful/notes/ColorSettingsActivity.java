99f32af4f285ebcc30b980f4cc3669603b566969
Matthew Conquergood <matthew.conquergood@gmail.com>
2015-05-17 18:57:34 -0700

    Add bgGroup local var.

--- app/src/main/java/com/thinkful/notes/ColorSettingsActivity.java
+++ app/src/main/java/com/thinkful/notes/ColorSettingsActivity.java
@@ -1,17 +1,21 @@
 package com.thinkful.notes;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 
-public class ColorSettingsActivity extends ActionBarActivity {
+public class ColorSettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_settings);
+
+        RadioGroup bgGroup = (RadioGroup) findViewById(R.id.BGRadio);
     }
 
     @Override
